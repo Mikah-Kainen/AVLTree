babab8a5085479a09e0a8392f3052a588c1fe661
Mikah Kainen <mikah@mltd.com>
2020-07-06 16:01:39 -0700

    Add Node Funtionality to AVL treee

--- AVLTree/AVLTree.cs
+++ AVLTree/AVLTree.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace AVLTree
 {
-    public class AVLTree<T>
+    public class AVLTree<T> where T : IComparable<T>
     {
+        Node<T> RootNode;
+
+        public AVLTree ()
+        {
+            RootNode = null;
+        }
+
+        public Node<T> FindParent(Node<T> targetNode)
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+
     }
 }
---
+++ AVLTree/Node.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVLTree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public Node<T> RightChild;
+        public Node<T> LeftChild;
+
+        public T Value { get; private set; }
+
+        public Node (T value)
+        {
+            Value = value;
+            RightChild = null;
+            LeftChild = null;
+        }
+
+        public int Height()
+        {
+            if(RightChild == null && LeftChild == null)
+            {
+                return 1;
+            }
+
+            else if(RightChild == null)
+            {
+                return LeftChild.Height() + 1;
+            }
+            else if(LeftChild == null)
+            {
+                return RightChild.Height() + 1;
+            }
+
+            else if(RightChild.Height() > LeftChild.Height())
+            {
+                return RightChild.Height() + 1;
+            }
+            else
+            {
+                return LeftChild.Height() + 1;
+            }
+        }
+
+        public int Balance()
+        {
+            if(RightChild == null)
+            {
+                if(LeftChild == null)
+                {
+                    return 0;
+                }
+                return -LeftChild.Height();
+            }
+            else if(LeftChild == null)
+            {
+                return RightChild.Height();
+            }
+            return RightChild.Height() - LeftChild.Height();
+        }
+
+        public bool IsLessThan(Node<T> targetNode)
+        {
+            if(Value.CompareTo(targetNode.Value) < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
